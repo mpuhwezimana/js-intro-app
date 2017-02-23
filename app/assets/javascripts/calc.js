ffaa93172e57a9f16513fc91917c479a4d845cf9
mpuhwezimana <mpuhwearsene.com>
2017-02-23 14:53:22 +0200

    finished to work on addition calculator and daylight colors

--- app/assets/javascripts/calc.js
+++ app/assets/javascripts/calc.js
@@ -1,1 +1,8 @@
-
+var sum = 0
+function add (number){
+  if (number === 'Reset'){
+    sum = 0; 
+  } else 
+    sum += number
+  document.getElementById("printout").innerHTML = sum;
+  }
--- app/assets/javascripts/daylight.js
+++ app/assets/javascripts/daylight.js
@@ -1,1 +1,11 @@
+function morning(color){
+  document.body.style.background = "yellow";
+}
 
+function afternoon(color){
+  document.body.style.background = "orange";
+}
+
+function evening(color){
+  document.body.style.background = "black";
+}
